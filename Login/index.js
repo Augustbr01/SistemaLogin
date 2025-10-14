9b630278a5a4e4e29cca5e29f64011c9a2ae74d4
Augusto Corrêa <augustogatticorrea777@gmail.com>
2025-10-13 20:35:44 -0400

    Add HTML, CSS, and JavaScript for user authentication forms and styles

--- Login/index.js
+++ Login/index.js
@@ -3,18 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const registerForm = document.querySelector('.register');
     const registerLink = document.querySelector('.login .auth-sign-up a');
     const loginLink = document.querySelector('.register .auth-sign-up a');
-    
+
     registerLink.addEventListener('click', (e) => {
-        e.preventDefault(); 
-        loginForm.classList.add('hidden');
-        registerForm.classList.remove('hidden');
+        e.preventDefault();
+        loginForm.classList.remove('active');
         registerForm.classList.add('active');
     });
 
     loginLink.addEventListener('click', (e) => {
         e.preventDefault();
-        registerForm.classList.add('hidden');
-        loginForm.classList.remove('hidden');
+        registerForm.classList.remove('active');
         loginForm.classList.add('active');
     });
 });
